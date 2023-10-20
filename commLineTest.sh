e004760b8e8e95800138b3e04b5fd43dcc626270
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-10-20 15:42:22 +0530

    Add files via upload

---
+++ commLineTest.sh
@@ -0,0 +1,18 @@
+
+
+echo "The script is : $0"
+
+echo "Total arguments passed : $#"
+
+echo "Actual arguments are : $*"
+
+echo "1 argument  : $1"
+echo "2 argument  : $2"
+echo "3 argument  : $3"
+echo "4 argument  : $4"
+echo "5 argument  : $5"
+echo "6 argument  : $6"
+echo "7 argument  : $7"
+echo "8 argument  : $8"
+echo "9 argument  : $9"
+echo "10argument  : ${10}"
